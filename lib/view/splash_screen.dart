5afcd33ad24042e29892c73d9dc4e8bce37e859e
Suman <sumaka37@gmail.com>
2024-12-12 21:17:31 +0545

    new page

--- lib/view/splash_screen.dart
+++ lib/view/splash_screen.dart
@@ -5,6 +5,35 @@ class SplashScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          const Spacer(), // Push the logo and text slightly down
+          Image.asset(
+            'assets/images/image_2.png', // Replace with your logo path
+            height: 220,
+          ),
+          const SizedBox(height: 20),
+          const Text(
+            'ENERGIZE NEPAL',
+            style: TextStyle(
+              fontSize: 28,
+              fontWeight: FontWeight.bold,
+              color:
+                  Color.fromARGB(255, 102, 141, 209), // Use a dark text color
+            ),
+          ),
+          const Spacer(), // Push the progress indicator slightly up
+          const CircularProgressIndicator(
+            valueColor:
+                AlwaysStoppedAnimation<Color>(Colors.blue), // Match style
+          ),
+          const SizedBox(height: 60), // Add padding at the bottom
+        ],
+      ),
+      backgroundColor: const Color.fromARGB(
+          255, 255, 255, 255), // Ensure a clean white background
+    );
   }
 }
