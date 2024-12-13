3e1b4fd432590113241f3e2c383254170ffee014
Suman <sumaka37@gmail.com>
2024-12-13 12:41:27 +0545

    changes

--- lib/view/signup_screen.dart
+++ lib/view/signup_screen.dart
@@ -9,136 +9,192 @@ class SignUpPage extends StatelessWidget {
     return Scaffold(
       backgroundColor: const Color.fromARGB(255, 255, 255, 255),
       body: SafeArea(
-        child: Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 20.0),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              const SizedBox(height: 20),
-              // Logo
-              Image.asset(
-                'assets/images/image_2.png', // Replace with your logo asset path
-                height: 220,
-              ),
-              const SizedBox(height: 30),
-              // Title
-              const Text(
-                'ENERGIZE NEPAL',
-                style: TextStyle(
-                    fontSize: 32,
-                    fontWeight: FontWeight.bold,
-                    letterSpacing: 1.2,
-                    color: Color.fromARGB(255, 102, 141, 209)),
-              ),
-              const SizedBox(height: 30),
-              // Subtitle
-              const Text(
-                'Create an account',
-                style: TextStyle(
-                  fontSize: 28,
-                  fontWeight: FontWeight.bold,
-                  color: Colors.black,
+        child: SingleChildScrollView(
+          // This makes the page scrollable
+          child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 20.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                const SizedBox(height: 20),
+                // Logo
+                Image.asset(
+                  'assets/images/image_2.png', // Replace with your logo asset path
+                  height: 220,
                 ),
-              ),
-              const SizedBox(height: 10),
-              const Text(
-                'Please fill this details to create an account',
-                style: TextStyle(
-                  fontSize: 14,
-                  color: Colors.lightBlueAccent,
-                ),
-                textAlign: TextAlign.center,
-              ),
-              const SizedBox(height: 30),
-              // Input Fields
-              TextField(
-                decoration: InputDecoration(
-                  prefixIcon: const Icon(
-                    Icons.person_outline,
-                    color: Colors.greenAccent,
-                  ),
-                  hintText: 'Enter Username',
-                  border: OutlineInputBorder(
-                    borderSide:
-                        const BorderSide(color: Colors.greenAccent, width: 2.0),
-                    borderRadius: BorderRadius.circular(8),
-                  ),
-                ),
-              ),
-              const SizedBox(height: 20),
-              TextField(
-                decoration: InputDecoration(
-                  prefixIcon: const Icon(Icons.email_outlined),
-                  hintText: 'Enter your mail',
-                  border: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(10),
-                  ),
-                ),
-              ),
-              const SizedBox(height: 20),
-              TextField(
-                obscureText: true,
-                decoration: InputDecoration(
-                  prefixIcon: const Icon(Icons.lock_outline),
-                  hintText: 'Enter Password',
-                  border: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(10),
-                  ),
-                ),
-              ),
-              const SizedBox(height: 30),
-              // Sign-Up Button
-              ElevatedButton(
-                onPressed: () {},
-                style: ElevatedButton.styleFrom(
-                  backgroundColor: Colors.green,
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(10),
-                  ),
-                  padding:
-                      const EdgeInsets.symmetric(vertical: 15, horizontal: 80),
-                ),
-                child: const Text(
-                  'Sign Up',
+                const SizedBox(height: 30),
+                // Title
+                const Text(
+                  'ENERGIZE NEPAL',
                   style: TextStyle(
-                    fontSize: 16,
+                      fontSize: 32,
+                      fontWeight: FontWeight.bold,
+                      letterSpacing: 1.2,
+                      color: Color.fromARGB(255, 102, 141, 209)),
+                ),
+                const SizedBox(height: 30),
+                // Subtitle
+                const Text(
+                  'Create an account',
+                  style: TextStyle(
+                    fontSize: 28,
                     fontWeight: FontWeight.bold,
-                    color: Colors.white,
+                    color: Colors.black,
                   ),
                 ),
-              ),
-              const SizedBox(height: 20),
-              // Login Link
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Text(
-                    'Already have account? ',
-                    style: TextStyle(
-                      color: Colors.grey[600],
-                      fontSize: 14,
+                const SizedBox(height: 10),
+                const Text(
+                  'Please fill this details to create an account',
+                  style: TextStyle(
+                    fontSize: 14,
+                    color: Colors.lightBlueAccent,
+                  ),
+                  textAlign: TextAlign.center,
+                ),
+                const SizedBox(height: 30),
+                // Input Fields
+                TextField(
+                  decoration: InputDecoration(
+                    prefixIcon: const Icon(
+                      Icons.person_outline,
+                      color: Colors.greenAccent,
+                    ),
+                    hintText: 'Enter Username',
+                    border: OutlineInputBorder(
+                      borderSide: const BorderSide(
+                          color: Colors.greenAccent, width: 2.0),
+                      borderRadius: BorderRadius.circular(8),
+                    ),
+                    enabledBorder: OutlineInputBorder(
+                      borderSide: const BorderSide(
+                          color: Colors.greenAccent, width: 2.0),
+                      borderRadius: BorderRadius.circular(8),
                     ),
                   ),
-                  GestureDetector(
-                    onTap: () {
-                      Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                            builder: (context) => const LoginScreen()),
+                ),
+                const SizedBox(height: 20),
+                TextField(
+                  decoration: InputDecoration(
+                    prefixIcon: const Icon(
+                      Icons.email_outlined,
+                      color: Colors.greenAccent,
+                    ),
+                    hintText: 'Enter your mail',
+                    border: OutlineInputBorder(
+                      borderSide: const BorderSide(
+                          color: Colors.greenAccent, width: 2.0),
+                      borderRadius: BorderRadius.circular(8),
+                    ),
+                    enabledBorder: OutlineInputBorder(
+                      borderSide: const BorderSide(
+                          color: Colors.greenAccent, width: 2.0),
+                      borderRadius: BorderRadius.circular(8),
+                    ),
+                  ),
+                ),
+                const SizedBox(height: 20),
+                TextField(
+                  obscureText: true,
+                  decoration: InputDecoration(
+                    prefixIcon: const Icon(
+                      Icons.lock_outline,
+                      color: Colors.greenAccent,
+                    ),
+                    hintText: 'Enter Password',
+                    border: OutlineInputBorder(
+                      borderSide: const BorderSide(
+                          color: Colors.greenAccent, width: 2.0),
+                      borderRadius: BorderRadius.circular(8),
+                    ),
+                    enabledBorder: OutlineInputBorder(
+                      borderSide: const BorderSide(
+                          color: Colors.greenAccent, width: 2.0),
+                      borderRadius: BorderRadius.circular(8),
+                    ),
+                  ),
+                ),
+                const SizedBox(height: 30),
+                // Sign-Up Button
+                SizedBox(
+                  width: double.infinity,
+                  child: ElevatedButton(
+                    onPressed: () {
+                      showDialog(
+                        context: context,
+                        builder: (BuildContext context) {
+                          return AlertDialog(
+                            title: const Text('Success!'),
+                            content: const Text(
+                                'Your account has been created successfully.'),
+                            actions: [
+                              TextButton(
+                                onPressed: () {
+                                  Navigator.of(context).pop();
+                                  Navigator.push(
+                                    context,
+                                    MaterialPageRoute(
+                                        builder: (context) =>
+                                            const LoginScreen()),
+                                  );
+                                },
+                                child: const Text('OK'),
+                              ),
+                            ],
+                          );
+                        },
                       );
                     },
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: Colors.green,
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(10),
+                      ),
+                      padding: const EdgeInsets.symmetric(vertical: 15),
+                    ),
                     child: const Text(
-                      'Log In',
+                      'Sign Up',
                       style: TextStyle(
-                        color: Colors.blue,
-                        fontSize: 14,
+                        fontSize: 16,
                         fontWeight: FontWeight.bold,
+                        color: Colors.white,
                       ),
                     ),
                   ),
-                ],
-              ),
-            ],
+                ),
+                const SizedBox(height: 20),
+                // Login Link
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text(
+                      'Already have account? ',
+                      style: TextStyle(
+                        color: Colors.grey[600],
+                        fontSize: 14,
+                      ),
+                    ),
+                    GestureDetector(
+                      onTap: () {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                              builder: (context) => const LoginScreen()),
+                        );
+                      },
+                      child: const Text(
+                        'Log In',
+                        style: TextStyle(
+                          color: Colors.blue,
+                          fontSize: 14,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+                const SizedBox(height: 20), // Extra space to prevent overlap
+              ],
+            ),
           ),
         ),
       ),
